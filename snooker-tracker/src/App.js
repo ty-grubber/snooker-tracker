53f4cd0493269c16d50e46cbe079860a58c6ab3d
Tyler Grubb <tgrubb@gasbuddy.com>
2020-08-25 16:48:43 -0600

    create app contents

---
+++ snooker-tracker/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './ball-ring.jpg'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Future home of the Snooker Tracker!!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
