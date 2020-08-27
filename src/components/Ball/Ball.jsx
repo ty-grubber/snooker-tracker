0ef387507da8f780fdc82219af840c99fad8c84c
Tyler Grubb <tgrubb@gasbuddy.com>
2020-08-26 19:59:49 -0600

    create skeleton components for Ball, Scoreboard, and Table

---
+++ src/components/Ball/Ball.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Ball() {
+  return (
+    <div>This is a Ball Component</div>
+  )
+}
---
+++ src/components/Scoreboard/Scoreboard.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Scoreboard() {
+  return (
+    <div>This is a Scoreboard Component</div>
+  )
+}
