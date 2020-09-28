222b98b6c75d79e937f7db418f1e42ef015d68a0
Tyler Grubb <tgrubb@gasbuddy.com>
2020-09-28 16:21:21 -0600

    feat: add baulk line and circle to table

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -5,25 +5,16 @@ export default function Table() {
   return (
     <div className="Table-outside">
       <div className="Table-bumpers">
+        <div className="Table-baulk circleTop" />
+        <div className="Table-baulk circleBottom" />
+        <div className="Table-baulk line" />
       </div>
-      <div id="left-top" className="Table-pocket">
-
-      </div>
-      <div id="center-top" className="Table-pocket">
-
-      </div>
-      <div id="right-top" className="Table-pocket">
-
-      </div>
-      <div id="left-bottom" className="Table-pocket">
-
-      </div>
-      <div id="center-bottom" className="Table-pocket">
-
-      </div>
-      <div id="right-bottom" className="Table-pocket">
-
-      </div>
+      <div id="left-top" className="Table-pocket" />
+      <div id="center-top" className="Table-pocket" />
+      <div id="right-top" className="Table-pocket" />
+      <div id="left-bottom" className="Table-pocket" />
+      <div id="center-bottom" className="Table-pocket" />
+      <div id="right-bottom" className="Table-pocket" />
     </div>
   )
 }
