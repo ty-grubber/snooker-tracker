33906fa495f23564d72c9ad1b79b4d476c3603fb
Tyler Grubb <tgrubb@gasbuddy.com>
2020-10-07 23:31:34 -0600

    feat: add mapping for all possible ball colors; create PinkBall for testing

--- src/components/Ball/Ball.jsx
+++ src/components/Ball/Ball.jsx
@@ -1,9 +1,21 @@
 import styled from '@emotion/styled';
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function Ball() {
+const COLOR_MAP = {
+  white: '#fff',
+  red: '#ce0317',
+  yellow: '#f9e137',
+  green: '#04541d',
+  brown: '#624024',
+  blue: '#0632d0',
+  pink: '#fb6284',
+  black: '#000',
+}
+
+export default function Ball({className, color}) {
   const BallDiv = styled.div`
-    background-color: green;
+    background-color: ${props => COLOR_MAP[props.color]};
     border: 1px solid black;
     height: 25px;
     width: 25px;
@@ -12,6 +24,16 @@ export default function Ball() {
   `
 
   return (
-    <BallDiv>&nbsp;</BallDiv>
-  )
+    <BallDiv className={className} color={color}>&nbsp;</BallDiv>
+  );
+}
+
+Ball.propTypes = {
+  className: PropTypes.string,
+  color: PropTypes.string,
+}
+
+Ball.defaultProps = {
+  className: undefined,
+  color: 'red',
 }
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,14 +1,22 @@
+import styled from '@emotion/styled';
 import React from 'react';
 import Ball from '../Ball';
 import './Table.css';
 
 export default function Table() {
+  const PinkBall = styled(Ball)`
+    left: 75%;
+    top: 50%;
+  `
   return (
     <div className="Table-outside">
-      <div className="Table-bumpers">
+      <div className="Table-inside">
         <div className="Table-baulk circleTop" />
         <div className="Table-baulk circleBottom" />
         <div className="Table-baulk line" />
+        <div className="Table-balls">
+          <PinkBall color="pink" />
+        </div>
       </div>
       <div id="left-top" className="Table-pocket" />
       <div id="center-top" className="Table-pocket" />
@@ -16,9 +24,6 @@ export default function Table() {
       <div id="left-bottom" className="Table-pocket" />
       <div id="center-bottom" className="Table-pocket" />
       <div id="right-bottom" className="Table-pocket" />
-      <div className="Table-balls">
-        <Ball />
-      </div>
     </div>
   )
 }
