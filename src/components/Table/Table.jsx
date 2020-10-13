e65b7cbfeda62128669721a5e5e146a9f0f529e6
Tyler Grubb <tgrubb@gasbuddy.com>
2020-10-13 14:05:58 -0600

    feat: style each BallMenu based on color of styled Ball

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -9,42 +9,78 @@ export default function Table() {
     background-color: #fff;
     left: calc(10% - calc(${sizing.ballDiameter} / 2));
     top: calc(50% - calc(${sizing.ballDiameter} / 2));
+
+    & > div {
+      background-color: #fff;
+    }
   `
   const YellowBall = styled(Ball)`
     background-color: #f9e137;
     left: calc(20% - calc(${sizing.ballDiameter} / 2));
     top: calc(75% - calc(${sizing.ballDiameter} / 2));
+
+    & > div {
+      background-color: #f9e137;
+    }
   `
   const GreenBall = styled(Ball)`
     background-color: #04541d;
     left: calc(20% - calc(${sizing.ballDiameter} / 2));
     top: calc(25% - calc(${sizing.ballDiameter} / 2));
+
+    & > div {
+      background-color: #04541d;
+      color: #fff;
+    }
   `
   const BrownBall = styled(Ball)`
     background-color: #624024;
     left: calc(20% - calc(${sizing.ballDiameter} / 2));
     top: calc(50% - calc(${sizing.ballDiameter} / 2));
+
+    & > div {
+      background-color: #624024;
+      color: #fff;
+    }
   `
   const BlueBall = styled(Ball)`
     background-color: #0632d0;
     left: calc(50% - calc(${sizing.ballDiameter} / 2));
     top: calc(50% - calc(${sizing.ballDiameter} / 2));
+
+    & > div {
+      background-color: #0632d0;
+      color: #fff;
+    }
   `
   const PinkBall = styled(Ball)`
     background-color: #fb6284;
     left: calc(70% - calc(${sizing.ballDiameter} / 2));
     top: calc(50% - calc(${sizing.ballDiameter} / 2));
+
+    & > div {
+      background-color: #fb6284;
+    }
   `
   const RedBall = styled(Ball)`
     background-color: #ce0317;
     left: calc(80% - calc(${sizing.ballDiameter} / 2));
     top: calc(50% - calc(${sizing.ballDiameter} / 2));
+
+    & > div {
+      background-color: #ce0317;
+      color: #fff;
+    }
   `
   const BlackBall = styled(Ball)`
     background-color: #000;
-    border: 1px solid #fff;
     left: calc(90% - calc(${sizing.ballDiameter} / 2));
     top: calc(50% - calc(${sizing.ballDiameter} / 2));
+
+    & > div {
+      background-color: #000;
+      color: #fff;
+    }
   `
   return (
     <div className="Table-outside">
